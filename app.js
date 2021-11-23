288e7b9b3425f31851bcd0ff4f1590e5f0737451
anita-brown <anita.osagie@decagon.dev>
2021-11-23 21:38:10 +0100

    ejs assignment

--- app.js
+++ app.js
@@ -1,41 +1,27 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+// Imports
+const express = require('express')
+const expressLayouts = require('express-ejs-layouts')
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const app = express()
+const port = 5008
 
-var app = express();
+// Static Files
+app.use(express.static('public'))
+app.use('/css', express.static(__dirname + 'public/css'))
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// Set Templating Engine
+app.use(expressLayouts)
+app.set('layout', './layouts/full-width')
+app.set('view engine', 'ejs')
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// Routes
+app.get('', (req, res) => {
+  res.render('index', { title: 'Home Page' })
+})
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.get('/about', (req, res) => {
+  res.render('about', { title: 'About Page', layout: './layouts/sidebar' })
+})
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
+// Listen on Port 5000
+app.listen(port, () => console.info(`App listening on port ${port}`))
